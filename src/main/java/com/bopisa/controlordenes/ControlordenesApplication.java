cc070e6221b49a23859adf2ae6fd930f88750790
aquinoedgar126-droid <aquinoedgar126@gmail.com>
2026-04-22 20:36:45 -0600

    Add files via upload

---
+++ src/main/java/com/bopisa/controlordenes/ControlordenesApplication.java
@@ -0,0 +1,13 @@
+package com.bopisa.controlordenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlordenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlordenesApplication.class, args);
+	}
+
+}
